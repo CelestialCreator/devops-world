e86defcf60ddde2f1f42c538a76f5197c80b1385
CelestialCreator <mhaskarakshay1992@gmail.com>
2026-04-09 17:21:04 +0530

    Add Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style/main.css'
+import { inject } from '@vercel/analytics'
 import Application from './javascript/Application.js'
+
+inject()
 
 window.application = new Application({
     $canvas: document.querySelector('.js-canvas'),
